9f820afba953bbde754baefaef24c038429f65ad
Olexandr Trykopa <55888517+trykopa@users.noreply.github.com>
2019-11-22 13:49:54 +0200

    HomeWork update
    2. Напишите метод который найдет в массиве целых чисел число модуль которого ближе всего к 0. Если есть два таких числа (например 2 и -2) верните положительное.
    3. На основе строки сгенерируйте массив целых чисел, каждое число должно быть ASCII кодом соответствующей буквы.
    4. Найдите максимальное число из набора чисел которые хранятся в текстовом файле.

--- StreamHW.java
+++ StreamHW.java
@@ -1,23 +1,40 @@
 package HomeWork10_3;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamHW {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// fill the array with 10 random integers from -100 to 100
 		int[] my = randomAr(10);
 		
 		int result = findMinAbs(my);
 		System.out.format("%n%d - is a number from Array with an ABS closest to 0%n%n", result);
 		
+		//String to stream of int 
 		String myStr = "The String class represents character strings.";
 		int[] array = myStr.chars().toArray();
 		System.out.println(Arrays.toString(array));
-
 		
+		
+		String intfile = "/Users/at/eclipse-workspace/oop/HomeWork10_3/intfile.txt";
+		System.out.format("Max int in %s found - %n%d", intfile, findMaxIntinFile(intfile));
+		
+	}
+	
+	public static int findMaxIntinFile (String file) throws IOException {
+		return Files.lines((new File(file)).toPath())
+			 	.flatMap( s -> Stream.of(s.split(" ")))
+				.map(Integer::valueOf)
+				.max(Comparator.naturalOrder())
+				.get();
 	}
 	
 	public static int[] randomAr(int lim) {
